69ae920389f6a33b4a7546e82473d7a97a25310d
Florian Asperger <flo.asperger@gmail.com>
2023-03-01 22:17:43 +0100

    doesn't work, will replace tkinter with qt for nicer ui, commit just to preserve code

--- main_window.py
+++ main_window.py
@@ -6,6 +6,7 @@ Classes:
 import tkinter as tk
 import tkinter.ttk as ttk
 from PIL import ImageTk, Image
+from ui.frames.frame_expandable import ExpandableFrame
 
 class MainWindow(tk.Tk):
     """
@@ -33,16 +34,20 @@ class MainWindow(tk.Tk):
         self.tk.call("set_theme", "dark")
         
         # Create a Frame for the Checkbuttons
-        control_frame = ttk.LabelFrame(self, text="Controls", padding=(20, 10))
-        control_frame.grid(row=0, column=0, padx=(20, 10), pady=(20, 10), sticky="nsew")
+        self.control_frame = ttk.LabelFrame(self, text="Controls", padding=(20, 10))
+        self.control_frame = ttk.Frame(self, padding=(20, 10))
+        self.control_frame.grid(row=0, column=0, padx=(20, 10), pady=(20, 10), sticky="nsew")
+        
+        self.load_controls()
         
         # Create a Frame for the Image
-        image_frame = ttk.LabelFrame(self, text="Image", padding=(20, 10))
-        image_frame.grid(row=0, column=1, padx=10, pady=(20, 10), sticky="nsew")
+        self.image_frame = ttk.LabelFrame(self, text="Image", padding=(20, 10))
+        self.image_frame.grid(row=0, column=1, padx=10, pady=(20, 10), sticky="nsew")
         
-        img = ImageTk.PhotoImage(Image.open("ui/icons/no-image-icon.png"))
-        panel = ttk.Label(image_frame, image=img)
-        panel.pack()
+        self.img = ImageTk.PhotoImage(Image.open("ui/icons/no-image-icon.png"))
+        panel = ttk.Label(self.image_frame, image=self.img)
+        panel.image = self.img
+        panel.grid(row=0, column=0, sticky="nesw")
         
         # self.resizable(False, False)
         # Center the window, and set minsize
@@ -53,5 +58,13 @@ class MainWindow(tk.Tk):
         self.geometry("+{}+{}".format(x_cordinate, y_cordinate))
         
         self.mainloop()
+    
+    def load_controls(self):
+        self.rowconfigure(index=0, weight=1)
+        self.columnconfigure(index=0, weight=1)
+        ref_frame = ExpandableFrame(self.control_frame)
+        ref_frame.grid(row=0, column=0, sticky="nesw")
+    
+    
         
     
